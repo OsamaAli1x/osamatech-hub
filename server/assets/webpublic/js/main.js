114262e27f9fe90893c4a2d82d4c1ccb3af2e846
Osama Ali Kawkabah <qu687599@gmail.com>
2026-07-20 08:59:51 +0300

    Improve website UI and update frontend assets

--- server/assets/webpublic/js/main.js
+++ server/assets/webpublic/js/main.js
@@ -18,15 +18,35 @@ function updateButton(element, commandID, additionalParams = {}) {
         // ok, yes, i'm adding 'fake' delay, it just makes the front end nicer, okay!?
         if (error) {
             setTimeout(() => {
-                showNotification('#f03434', error)
+                showNotification('#ff0000', error)
                 $(element).removeClass('loading')
             }, 300)
         } else {
             setTimeout(() => {
-                showNotification('#2ecc71', message);
+                showNotification('#00ff41', message);
                 $(element).removeClass('loading');
                 if (message === 'Requested') setTimeout(() => { window.location = window.location }, 200)
             }, 300)
         }
     });
 }
+
+// Typewriter welcome text simulator
+$(document).ready(function() {
+    var welcomeMsg = "Welcome to OsamaAli Command Center...";
+    var speed = 80;
+    var i = 0;
+    var target = document.getElementById("welcome-msg");
+    
+    if (target) {
+        function typeWriter() {
+            if (i < welcomeMsg.length) {
+                target.innerHTML += welcomeMsg.charAt(i);
+                i++;
+                setTimeout(typeWriter, speed);
+            }
+        }
+        // Small delay before typewriter starts
+        setTimeout(typeWriter, 500);
+    }
+});
